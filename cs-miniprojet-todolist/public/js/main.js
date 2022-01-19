a84f085571863dfaaa671e8ed264f44c379f179e
Kevin <blisteinkevin@outlook.com>
2022-01-19 09:10:21 +0100

    toDoList delete_function_icon

--- cs-miniprojet-todolist/public/js/main.js
+++ cs-miniprojet-todolist/public/js/main.js
@@ -3,25 +3,59 @@ let add = document.querySelector(".add");
 
 let tasks = document.querySelector(".tasks")
 
+// Ajouter une tâche
+
 let ajout = () => {
-    let newTask = document.createElement("div");
+    var newTask = document.createElement("div");
     newTask.style.display = "flex";
     newTask.style.justifyContent = "space-around"
-    let newCheck = document.createElement("input");
+    var newCheck = document.createElement("input");
     newCheck.type = "checkbox"
     console.log(newCheck)
-    let newP = document.createElement("p");
-    let newTrashIcon = document.createElement("i");
+    var newP = document.createElement("p");
+    var newTrashIcon = document.createElement("i");
     newTrashIcon.className = "fas fa-trash-alt";
-    let newEditIcon = document.createElement("i");
-    newEditIcon.className = "fas fa-edit"; 
+    var newEditIcon = document.createElement("i");
+    newEditIcon.className = "fas fa-edit";
     tasks.appendChild(newTask);
     newTask.appendChild(newCheck);
     newTask.appendChild(newP);
     newP.innerText = input.value;
     newTask.appendChild(newTrashIcon);
     newTask.appendChild(newEditIcon);
+    input.value = "";
+
+    // Fonction supprimer
+
+    let trashIcon = [...document.querySelectorAll(".fa-trash-alt")];
+    console.log(trashIcon);
+
+    let supprimer = (e) => {
+        console.log(e.target.parentElement);
+        e.target.parentElement.remove();
+    }
+
+    trashIcon.forEach(element => {
+        trashIcon[trashIcon.indexOf(element)].addEventListener("click", supprimer);
+    })
+
+    // modifier le nom d'une tâche
+
+    let editIcon = [...document.querySelectorAll(".fas fa-edit")]
+    console.log(editIcon);
+    let edit = () => {
+        let newInput = document.createElement("input");
+        newTask.prependChild(newInput);
+
+    }
+
+    newEditIcon.addEventListener("dblclick", edit);
+
+
 }
 
 
 add.addEventListener("click", ajout)
+
+
+
